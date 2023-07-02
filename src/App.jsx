3cb05fb326e21ba0375c51a3e9fc1cb7c185be69
preston176 <prestonnyamweya@gmail.com>
2023-07-02 18:18:46 +0300

    next car section + styling

--- src/App.jsx
+++ src/App.jsx
@@ -113,6 +113,54 @@ function App() {
         </div>
       </div>
      </div>
+
+     <div className="best-deals">
+     <div className="best-deals-h1">
+     <h1>Choose your next car</h1>
+     </div>
+      <div className="cars-container">
+        <div className="cars-next">
+          <div className="cars-img">
+            <img src="../public/home.png" alt="" />
+          </div>
+          <div className="cars-price">From </div>
+          <div className="btn-book-now-2"><button>Book Now</button></div>
+        </div>
+
+        <div className="cars-next">
+          <div className="cars-img">
+            <img src="../public/home.png" alt="" />
+          </div>
+          <div className="cars-price">From </div>
+          <div className="btn-book-now-2"><button>Book Now</button></div>
+        </div>
+
+
+        <div className="cars-next">
+          <div className="cars-img">
+            <img src="../public/home.png" alt="" />
+          </div>
+          <div className="cars-price">From </div>
+          <div className="btn-book-now-2"><button>Book Now</button></div>
+        </div>
+
+        <div className="cars-next">
+          <div className="cars-img">
+            <img src="../public/home.png" alt="" />
+          </div>
+          <div className="cars-price">From </div>
+          <div className="btn-book-now-2"><button>Book Now</button></div>
+        </div>
+
+        <div className="cars-next">
+          <div className="cars-img">
+            <img src="../public/home.png" alt="" />
+          </div>
+          <div className="cars-price">From </div>
+          <div className="btn-book-now-2"><button>Book Now</button></div>
+        </div>
+      </div>
+     </div>
     </>
   )
 }

38a94f78b25064f0e8b8f3ab7e7596473f9e8062
preston176 <prestonnyamweya@gmail.com>
2023-07-02 20:39:23 +0300

    image not displaying

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -4,8 +4,8 @@ export default [
         id: 1,
         title: "Bugatti",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-        price: 136,
-        coverImg: 'benz.png',
+        price: `200,000`,
+        coverImg: '/benz.png',
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -16,8 +16,8 @@ export default [
     {
         id: 2,
         title: "lambo",
-        price: 125,
-        coverImg: "lambo.png",
+        price: `700,000`,
+        coverImg: "/lambo.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -29,13 +29,13 @@ export default [
         id: 3,
         title: "Audi",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "auditt.png",
+        price: `180,000`,
+        coverImg: "/auditt.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
         },
-        location: "Norway",
+        location: "/Norway",
         openSpots: 3,
     }
 ]

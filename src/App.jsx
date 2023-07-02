c0b71e54bae1966a91dbfe8a053fbee17b51f7c8
preston176 <prestonnyamweya@gmail.com>
2023-07-02 13:45:44 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,34 @@ function App() {
 
   return (
     <>
-      <div style={{width: 487, height: 1067, background: 'linear-gradient(180deg, #F6F7F9 0%, #E9ECF5 100%)', borderRadius: 50}} />
+     <div className="Header">
+        <img src={reactLogo} className="Header-logo" alt="logo" />
+        <nav className='navbar'>
+        <li>Homepage</li>
+        <li>Reservation</li>
+        <li>Locations</li>
+        <li>Fleet</li>
+        <li>Special Offers</li>
+        </nav>
+     </div>
+     <div className="main">
+      <div className="main-left">
+        <h1>Book your car today!</h1>
+        <p>Whether you're planning a road trip, need a reliable vehicle for a business trip, or just want the convenience of having a car at your disposal, we've got you covered</p>
+        <div className="main-left-section">
+        <button>Book Now</button>
+        <a>See all cars</a>&#8594;
+        </div>
+      </div>
+      <div className="main-right">
+        <h2>Reserve your car in 3 easy steps</h2>
+     </div>
+     </div>
+     <div className="sponsors"></div>
+     <div className="testimony"></div>
+     <div className="best-deals">
+      <div className="car-image-container"></div>
+     </div>
     </>
   )
 }

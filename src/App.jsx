1480fc367e00c42ce1f3e8d8f9b926e686973b53
preston176 <prestonnyamweya@gmail.com>
2023-07-02 14:56:16 +0300

    structure layout + responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,24 @@ function App() {
      </div>
      <div className="main">
       <div className="main-left">
-        <h1>Book your car today!</h1>
-        <p>Whether you're planning a road trip, need a reliable vehicle for a business trip, or just want the convenience of having a car at your disposal, we've got you covered</p>
+        <h1 className='main-left-text'>Explore the freedom of car rental with <span className='main-left-text-span'>Drivewise.</span></h1>
+        <p className='main-left-text-p'>Whether you're planning a road trip, need a reliable vehicle for a business trip, or just want the convenience of having a car at your disposal, we've got you covered</p>
         <div className="main-left-section">
-        <button>Book Now</button>
-        <a>See all cars</a>&#8594;
+        <button className='main-left-btn'>Book Now</button>
+        <a className='main-left-see-all'>See all cars&#8594;</a>
         </div>
       </div>
       <div className="main-right">
-        <h2>Reserve your car in 3 easy steps</h2>
+      <div className="main-right-img">
+        <img className='homeimg' src="../public/home.png" alt="" />
+        </div>
      </div>
      </div>
-     <div className="sponsors"></div>
+     <div className="sponsors">
+      <div className="car-brands">
+        <img src="../public/cars.png" alt="" />
+      </div>
+     </div>
      <div className="testimony"></div>
      <div className="best-deals">
       <div className="car-image-container"></div>

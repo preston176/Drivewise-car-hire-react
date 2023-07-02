5f6d3322b53c1fe422eebedbb61b742c1d67a9e6
preston176 <prestonnyamweya@gmail.com>
2023-07-02 13:02:56 +0300

    phone ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{width: 487, height: 1067, background: 'linear-gradient(180deg, #F6F7F9 0%, #E9ECF5 100%)', borderRadius: 50}} />
     </>
   )
 }
